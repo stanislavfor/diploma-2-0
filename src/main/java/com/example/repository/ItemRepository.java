135197590c613cb30d875aed936710e658c50f6f
stanislavfor <stasposter@narod.ru>
2024-08-25 02:48:47 +0300

    first commit

---
+++ src/main/java/com/example/repository/ItemRepository.java
@@ -0,0 +1,9 @@
+package com.example.repository;
+
+import com.example.model.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+}
+
+
---
+++ src/main/java/com/example/service/ImageService.java
@@ -0,0 +1,28 @@
+package com.example.service;
+
+import com.example.model.Item;
+import com.example.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ImageService {
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    public List<Item> getAllItems() {
+        return itemRepository.findAll();
+    }
+
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public void deleteItem(Long id) {
+        itemRepository.deleteById(id);
+    }
+}
+
